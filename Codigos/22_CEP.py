ed8676d28c67e83db24497ac496dcf8448a45042
Guilherme Romanowski Barbosa <guilherme_romanowski_@hotmail.com>
2026-01-11 10:02:58 -0300

    Conclusão da Aula 12 - Consumindo dados de API.

--- Codigos/22_CEP.py
+++ Codigos/22_CEP.py
@@ -1,6 +1,11 @@
 #%%
 # request é a biblioteca para consumir dados de API
+# Json para tratar listas/dicionários para arquivos JSON
 import requests
+import json 
+from tqdm import tqdm # Faz a barra de progresso
+import pandas as pd
+
 
 ceps = [
     "01519000",
@@ -20,18 +25,27 @@ ceps = [
 url = "https://viacep.com.br/ws/{ceps}/json/"
 dados = []
 
-for i in ceps:
+for i in tqdm(ceps):
     resposta = requests.get(url.format(ceps=i))
     if resposta.status_code == 200:
         dados.append(resposta.json())
 
 dados
-
 #.get é o mesmo que jogar o link no navegador. Ele requisita o que tem dentro da URL.
 #Alguns tipos abaixo de retorno dos dados.
-#resposta #-> resposta 200 = ok
+#resposta -> resposta 200 = ok
 #resposta.raw
 #resposta.text
 #resposta.json()
+
+
+dataset = pd.DataFrame(dados)
+dataset.to_csv("ceps.csv",sep=";")
+
+#também podemos salvar o arquivo com os dados do print acima:
+with open("ceps.json", "w",encoding='UTF-8') as open_file:
+    json.dump(dados,open_file,ensure_ascii=False,indent= 4)
+
+
 
 # %%
